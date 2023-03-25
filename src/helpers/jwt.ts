51cbba3143e82089b50bb94746ee78ae49bc1fad
anandafiqri <anandafiqri1403@gmail.com>
2023-03-25 17:13:05 +0700

    fix

--- src/helpers/jwt.ts
+++ src/helpers/jwt.ts
@@ -2,7 +2,7 @@ import { JwtPayload, Secret, verify, sign } from "jsonwebtoken";
 export const secret: Secret = process.env.secret as Secret;
 
 export const createToken = (payload: object): string =>
-  sign(payload, secret, { algorithm: "ES256" });
+  sign(payload, secret, { algorithm: "HS256" });
 
 export const verifyToken = (token: string | any): JwtPayload | any =>
-  verify(token, secret, { algorithms: ["ES256"] });
+  verify(token, secret, { algorithms: ["HS256"] });
