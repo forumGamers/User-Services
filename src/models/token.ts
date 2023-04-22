d075d9ec9028da2d423de61a152f7843556ffbca
anandafiqri <anandafiqri1403@gmail.com>
2023-04-22 10:23:24 +0700

    create token table

---
+++ src/models/token.ts
@@ -0,0 +1,56 @@
+import {
+  Sequelize,
+  Model,
+  DataTypes,
+  BelongsToCreateAssociationMixin,
+  BelongsToGetAssociationMixin,
+  BelongsToSetAssociationMixin,
+  BelongsToSetAssociationMixinOptions,
+} from "sequelize";
+import User from "./user";
+
+export default class Achievement extends Model {
+  public static associate(models: any) {
+    Achievement.belongsTo(models.User, { foreignKey: "UserId" });
+  }
+
+  public access_token!: string;
+  public role!: string;
+  public UserId!: number;
+
+  public addUser!: BelongsToCreateAssociationMixin<User>;
+  public addUsers!: BelongsToGetAssociationMixin<User>;
+  public createUser!: BelongsToCreateAssociationMixin<User>;
+  public getUsers!: BelongsToGetAssociationMixin<User>;
+  public hasUser!: BelongsToGetAssociationMixin<User>;
+
+  public static initialize(sequelize: Sequelize) {
+    this.init(
+      {
+        access_token: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        UserId: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: {
+              tableName: "Users",
+            },
+            key: "id",
+          },
+          onDelete: "CASCADE",
+          onUpdate: "CASCADE",
+        },
+        role: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        modelName: "Token",
+      }
+    );
+  }
+}
